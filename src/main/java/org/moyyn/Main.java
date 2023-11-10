e1cbdf8af5b0e79f8675ebb7b12b9302a2cbe473
SolarScripter <dippy.018@gmail.com>
2023-11-10 20:01:29 +0100

    Removed the comments

--- src/main/java/org/moyyn/Main.java
+++ src/main/java/org/moyyn/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
        int number = 1;
         while (number<=10) {
-            System.out.println(number);
+           // System.out.println(number);
             number++;
         }
 
