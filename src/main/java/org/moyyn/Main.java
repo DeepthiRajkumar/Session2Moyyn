c59f7ae6074b07fc9a7fcd982b2c227de3f69955
SolarScripter <dippy.018@gmail.com>
2023-11-10 19:54:13 +0100

    Removed the comments

--- src/main/java/org/moyyn/Main.java
+++ src/main/java/org/moyyn/Main.java
@@ -7,15 +7,15 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         //Print numbers from 1-10
-     /*   for (int i = 1; i<=10;i++) {
+      for (int i = 1; i<=10;i++) {
             System.out.println(i);
-        } */
+        }
 
-     /*   int number = 1;
+       int number = 1;
         while (number<=10) {
             System.out.println(number);
             number++;
-        } */
+        }
 
         int i =1 ;
         do {
